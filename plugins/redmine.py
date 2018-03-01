fd79aff0cd4b8adb9342263398f38d4bb095a1fb
Timothy Ball <timball+github@gmail.com>
2018-02-28 23:14:14 -0500

    better regexes are simpiler

--- plugins/redmine.py
+++ plugins/redmine.py
@@ -1,13 +1,16 @@
+
 import local_settings as conf
-import pprint
+import re
 
 from mattermost_bot.bot import listen_to
 from mattermost_bot.bot import respond_to
+regex = r'\#(\d+)'
 
 
-@listen_to('\W?\#(\d+)\#?(\d+)?\W?')
-def redmine_issue_responder(message, number, foo):
-    pp = pprint.PrettyPrinter(indent=4)
-    msg = ' https://redmine.equalit.ie/issues/' + number
+@listen_to(regex)
+def redmine_issue_responder(message, number):
+    url = "https://redmine.equalit.ie/issues/%s"
+
+    l = re.findall(regex, message.get_message())
+    msg = '\n'.join([url % (x) for x in l])
     message.reply(msg)
-    pp.pprint("XXX: " + number + " foo: " + str(foo) + " string: " + message.get_message())
